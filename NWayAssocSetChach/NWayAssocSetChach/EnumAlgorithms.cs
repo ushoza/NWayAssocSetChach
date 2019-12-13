9ac810daca19bf32811b2ae59671c62240bef87d
ushoza <ushoza@hotmail.ru>
2019-12-13 11:25:42 +0500

    Add Enum Algorithms To secondBranch

---
+++ NWayAssocSetChach/NWayAssocSetChach/EnumAlgorithms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NWayAssocSetChach
+{
+    public enum EnumAlgorithms
+    {
+        LRU,
+        MRU,
+        Castom
+    }
+}
